2fa53396a638b53d3c3670636f527b68121621c3
Chris Noreikis <chris.nore@gmail.com>
2021-09-23 17:27:22 -0500

    Using a @Bean to run code when CryptoTradingSimulatorApplication's main method is run

--- src/main/java/edu/depaul/cdm/se452/cryptotradingsimulator/CryptoTradingSimulatorApplication.java
+++ src/main/java/edu/depaul/cdm/se452/cryptotradingsimulator/CryptoTradingSimulatorApplication.java
@@ -1,10 +1,22 @@
 package edu.depaul.cdm.se452.cryptotradingsimulator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CryptoTradingSimulatorApplication {
+	private static final Logger log = LoggerFactory.getLogger(CryptoTradingSimulatorApplication.class);
+
+	@Bean
+	public CommandLineRunner printHello() {
+		return (args) -> {
+			log.info("Hello world");
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoTradingSimulatorApplication.class, args);
