bac87d92c2b85fe73bed4539ec64cdfa73910cce
Chris Noreikis <chris.nore@gmail.com>
2021-10-21 07:57:47 -0500

    Buy/sell page, portfolio list/view page

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,17 @@
+const getQuantityElement = () => document.querySelector('#quantity');
+const getCryptoDropdown = () => document.querySelector('#cryptocurrency-dropdown');
+
+function setTransactionValue(quantityElement, cryptoDropdown) {
+  let selectedPrice = cryptoDropdown.options[cryptoDropdown.selectedIndex].dataset.price;
+  if (quantityElement.value && selectedPrice) {
+    document.querySelector('#transaction-value').value = parseFloat(quantityElement.value) * parseFloat(selectedPrice);
+  }
+}
+
+getCryptoDropdown().addEventListener('change', (e) => {
+  setTransactionValue(getQuantityElement(), getCryptoDropdown());
+})
+
+getQuantityElement().addEventListener('change', (e) => {
+  setTransactionValue(getQuantityElement(), getCryptoDropdown());
+})
